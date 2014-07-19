bdbd32899b0b80b4e4464df32f6fd6fb8700a77f
mapk <uraine@gmail.com>
2014-07-19 16:22:55 -0700

    commented msg for later use of timestamps

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
 	def index
-		@jobs = Job.where(publish: true)
+		@jobs = Job.where(publish: true) #.where('published_at > ?', 3.months.ago)
 	end
 
 	def show
